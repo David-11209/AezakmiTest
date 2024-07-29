a23a087eb73fd35121dd99ab2d6fc0881fa54bc6
David-11209 <limonchik087@gmail.com>
2024-07-29 21:45:46 +0500

    add dependencies and AppDelegate

---
+++ AezakmiTest/AezakmiTest/AezakmiTestApp.swift
@@ -0,0 +1,21 @@
+//
+//  AezakmiTestApp.swift
+//  AezakmiTest
+//
+//  Created by Давид Васильев on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AezakmiTestApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
